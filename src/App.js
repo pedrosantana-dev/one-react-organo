8d1655cdba944980a29fba6134f52f74d6012ad4
Pedro Santana <pedromanu27@gmail.com>
2024-06-03 14:08:44 -0300

    feat: :sparkles: Componente Banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './componentes/Banner/Banner';
 
 function App() {
 	return (
 		<div className="App">
+			<Banner />
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
+				<p>Bem-vindo ao React!</p>
 				<p>
 					Edite <code>src/App.js</code> e salve para recarregar.
 				</p>
